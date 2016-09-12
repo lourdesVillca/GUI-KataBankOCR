e0430f0f554a4102e928f29a5c288167b01fdc2f
Lourdes Villca Lazaro <LourdesVillca@Fundacion-jala.local>
2016-09-12 10:50:56 -0400

    First commit - solve problem

---
+++ src/main/java/org/fundacionjala/katabank/BankORC.java
@@ -0,0 +1,72 @@
+package org.fundacionjala.katabank;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Class to format the number
+ */
+public class BankORC {
+
+    private static final Map<Integer, String> numberData = new HashMap<Integer, String>();
+
+    static {
+        numberData.put(1, "     |  |");
+        numberData.put(2, " _  _||_ ");
+        numberData.put(3, " _  _| _|");
+        numberData.put(4, "   |_|  |");
+        numberData.put(5, " _ |_  _|");
+        numberData.put(6, " _ |_ |_|");
+        numberData.put(7, " _   |  |");
+        numberData.put(8, " _ |_||_|");
+        numberData.put(9, " _ |_| _|");
+        numberData.put(0, " _ | ||_|");
+    }
+
+    /**
+     * Method to Parse the number read from the file
+     * @param number the string number
+     * @return the parse number
+     */
+    public static String parseNumber(String number) {
+        String numberLine1 = number.substring(0, 27);
+        String numberLine2 = number.substring(27, 54);
+        String numberLine3 = number.substring(54);
+        String result;
+        StringBuilder formatNumber = new StringBuilder();
+
+        for (int i = 0; i < numberLine1.length() - 2; i += 3) {
+            result = numberLine1.substring(i, i + 3) + numberLine2.substring(i, i + 3) + numberLine3.substring(i, i + 3);
+            formatNumber = (getKeysForNumber(result) == -1)?formatNumber.append("?"):formatNumber.append(getKeysForNumber(result));
+
+        }
+        return formatNumber.toString();
+    }
+
+    /**
+     * Method that search the key for a given Value
+     * @param value the number expression in String format
+     * @return the key of the value if the given value doesn't exist return -1
+     */
+    public static int getKeysForNumber(String value) {
+        return numberData.entrySet()
+                .stream()
+                .filter(entry -> entry.getValue().equals(value))
+                .map(Map.Entry::getKey)
+                .findFirst()
+                .orElse(-1);
+    }
+
+    /**
+     * Validate if the given account is valid
+     * @param accountNumber
+     * @return true if the account is valid
+     */
+    public static boolean validateAccountNumber(String accountNumber) {
+        int checksum = 0;
+        for (int i = 0; i < accountNumber.length(); i++) {
+            checksum += Character.getNumericValue(accountNumber.charAt(i))*(accountNumber.length()-i);
+        }
+        return checksum % 11 == 0;
+    }
+}
---
+++ src/test/java/org/fundacionjala/katabank/BankORCTest.java
@@ -0,0 +1,43 @@
+package org.fundacionjala.katabank;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Test for {@link BankORC}
+ */
+public class BankORCTest {
+    @Test
+    public void test_parseAllNumbers(){
+        final StringBuilder number = new StringBuilder();
+        number.append("    _  _     _  _  _  _  _ ");
+        number.append("  | _| _||_||_ |_   ||_||_|");
+        number.append("  ||_  _|  | _||_|  ||_| _|");
+        String result = BankORC.parseNumber(number.toString());
+        assertEquals("123456789", result);
+    }
+    @Test
+    public void test_validAccountNumber(){
+        String accountNumber = "345882865";
+        boolean actualResult = BankORC.validateAccountNumber(accountNumber);
+        assertTrue(actualResult);
+    }
+    @Test
+    public void test_invalidAccountNumber(){
+        String accountNumber = "664371495";
+        boolean actualResult = BankORC.validateAccountNumber(accountNumber);
+        assertFalse(actualResult);
+    }
+    @Test
+    public void test_illegibleAccountNumber(){
+        final StringBuilder number = new StringBuilder();
+        number.append("    _  _  _  _  _  _     _ ");
+        number.append("|_||_|| || ||_   |  |  | _ ");
+        number.append("  | _||_||_||_|  |  |  | _|");
+        String result = BankORC.parseNumber(number.toString());
+        assertEquals("49006771?", result);
+    }
+}
