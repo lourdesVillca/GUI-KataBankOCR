7fa5eb4465ede396bab3e44b29d8a6b6774648bb
lourdesVillca <lourdes.villca@fundacion-jala.org>
2016-10-05 23:25:51 -0400

    Add checkstyle, fix comments

--- src/main/java/org/fundacionjala/katabank/BankORC.java
+++ src/main/java/org/fundacionjala/katabank/BankORC.java
@@ -4,57 +4,89 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Class to format the number
+ * Class to format the number.
  */
-public class BankORC {
+public final class BankORC {
 
-    private static final Map<Integer, String> numberData = new HashMap<Integer, String>();
+    private static final Map<Integer, String> NUMBER_DATA = new HashMap<Integer, String>();
     private static final String ILLEGIBLE_CHARACTER = "?";
     private static final String ILL = " ILL";
     private static final String ERR = " ERR";
 
+    private static final int ONE = 1;
+
+    private static final int TWO = 2;
+
+    private static final int THREE = 3;
+
+    private static final int FOUR = 4;
+
+    private static final int FIVE = 5;
+
+    private static final int SIX = 6;
+
+    private static final int SEVEN = 7;
+
+    private static final int EIGHT = 8;
+
+    private static final int NINE = 9;
+
+    private static final int ZERO = 0;
+
+
     static {
-        numberData.put(1, "     |  |");
-        numberData.put(2, " _  _||_ ");
-        numberData.put(3, " _  _| _|");
-        numberData.put(4, "   |_|  |");
-        numberData.put(5, " _ |_  _|");
-        numberData.put(6, " _ |_ |_|");
-        numberData.put(7, " _   |  |");
-        numberData.put(8, " _ |_||_|");
-        numberData.put(9, " _ |_| _|");
-        numberData.put(0, " _ | ||_|");
+        NUMBER_DATA.put(ONE, "     |  |");
+        NUMBER_DATA.put(TWO, " _  _||_ ");
+        NUMBER_DATA.put(THREE, " _  _| _|");
+        NUMBER_DATA.put(FOUR, "   |_|  |");
+        NUMBER_DATA.put(FIVE, " _ |_  _|");
+        NUMBER_DATA.put(SIX, " _ |_ |_|");
+        NUMBER_DATA.put(SEVEN, " _   |  |");
+        NUMBER_DATA.put(EIGHT, " _ |_||_|");
+        NUMBER_DATA.put(NINE, " _ |_| _|");
+        NUMBER_DATA.put(ZERO, " _ | ||_|");
     }
 
+    /**
+     * Private Constructor.
+     */
     private BankORC() {
 
     }
 
     /**
-     * Method to Parse the number read from the file
+     * Method to Parse the number read from the file.
      *
-     * @param number the string number
-     * @return the parse number
+     * @param number the string number.
+     * @return the parse number.
      */
-    public static String parseNumber(String number) {
-        String numberLine1 = number.substring(0, 27);
-        String numberLine2 = number.substring(27, 54);
-        String numberLine3 = number.substring(54);
+    public static String parseNumber(final String number) {
+        final int intervalSpace = 3;
+        final int endIndexFirstLine = 27;
+        final int endIndexSecondLine = 54;
+        String numberLine1 = number.substring(0, endIndexFirstLine);
+        String numberLine2 = number.substring(endIndexFirstLine, endIndexSecondLine);
+        String numberLine3 = number.substring(endIndexSecondLine);
         StringBuilder formatNumber = new StringBuilder();
-        for (int i = 0; i < numberLine1.length() - 2; i += 3) {
-            String result = numberLine1.substring(i, i + 3) + numberLine2.substring(i, i + 3) + numberLine3.substring(i, i + 3);
-            formatNumber = (getKeysForNumber(result) == -1) ? formatNumber.append(ILLEGIBLE_CHARACTER) : formatNumber.append(getKeysForNumber(result));
+        for (int i = 0; i < numberLine1.length() - 2; i += intervalSpace) {
+            String result = numberLine1.substring(i, i + intervalSpace) + numberLine2.substring(i, i + intervalSpace)
+                    + numberLine3.substring(i, i + intervalSpace);
+            if (getKeysForNumber(result) == -1) {
+                formatNumber = formatNumber.append(ILLEGIBLE_CHARACTER);
+            } else {
+                formatNumber = formatNumber.append(getKeysForNumber(result));
+            }
         }
         return generateFormatNumber(formatNumber);
     }
 
     /**
-     * Method to generate the correct format for a parsed account number
+     * Method to generate the correct format for a parsed account number.
      *
-     * @param formatNumber parsed account number
-     * @return the parsed number account
+     * @param formatNumber parsed account number.
+     * @return the parsed number account.
      */
-    public static String generateFormatNumber(StringBuilder formatNumber) {
+    public static String generateFormatNumber(final StringBuilder formatNumber) {
         if (String.valueOf(formatNumber).contains(ILLEGIBLE_CHARACTER)) {
             formatNumber.append(ILL);
         } else if (!validateAccountNumber(formatNumber.toString())) {
@@ -64,13 +96,13 @@ public class BankORC {
     }
 
     /**
-     * Method that search the key for a given Value
+     * Method that search the key for a given Value.
      *
-     * @param value the number expression in String format
-     * @return the key of the value if the given value doesn't exist return -1
+     * @param value the number expression in String format.
+     * @return the key of the value if the given value doesn't exist return -1.
      */
-    public static int getKeysForNumber(String value) {
-        return numberData.entrySet()
+    public static int getKeysForNumber(final String value) {
+        return NUMBER_DATA.entrySet()
                 .stream()
                 .filter(entry -> entry.getValue().equals(value))
                 .map(Map.Entry::getKey)
@@ -79,16 +111,17 @@ public class BankORC {
     }
 
     /**
-     * Validate if the given account is valid
+     * Validate if the given account is valid.
      *
-     * @param accountNumber account number to validate
-     * @return true if the account is valid
+     * @param accountNumber account number to validate.
+     * @return true if the account is valid.
      */
-    public static boolean validateAccountNumber(String accountNumber) {
+    public static boolean validateAccountNumber(final String accountNumber) {
         int checksum = 0;
+        final int checksumDivider = 11;
         for (int i = 0; i < accountNumber.length(); i++) {
             checksum += Character.getNumericValue(accountNumber.charAt(i)) * (accountNumber.length() - i);
         }
-        return checksum % 11 == 0;
+        return checksum % checksumDivider == 0;
     }
 }
--- src/test/java/org/fundacionjala/katabank/BankORCTest.java
+++ src/test/java/org/fundacionjala/katabank/BankORCTest.java
@@ -7,11 +7,15 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 /**
- * Test for {@link BankORC}
+ * Test for {@link BankORC}.
  */
 public class BankORCTest {
+
+    /**
+     * Method to parse all numbers.
+     */
     @Test
-    public void test_parseAllNumbers() {
+    public void testParseAllNumbers() {
         final StringBuilder number = new StringBuilder();
         number.append("    _  _     _  _  _  _  _ ");
         number.append("  | _| _||_||_ |_   ||_||_|");
@@ -20,8 +24,11 @@ public class BankORCTest {
         assertEquals("123456789", result);
     }
 
+    /**
+     * Method to validate the result of the Invalid parsed account number.
+     */
     @Test
-    public void test_parseInvalidAccountNumber() {
+    public void testParseInvalidAccountNumber() {
         final StringBuilder number = new StringBuilder();
         number.append(" _  _     _  _        _  _ ");
         number.append("|_ |_ |_| _|  |  ||_||_||_ ");
@@ -30,22 +37,31 @@ public class BankORCTest {
         assertEquals("664371495 ERR", result);
     }
 
+    /**
+     * Method to validate valid account number.
+     */
     @Test
-    public void test_validAccountNumber() {
+    public void testValidAccountNumber() {
         final String accountNumber = "345882865";
         boolean actualResult = BankORC.validateAccountNumber(accountNumber);
         assertTrue(actualResult);
     }
 
+    /**
+     * Method to validate invalid account number.
+     */
     @Test
-    public void test_invalidAccountNumber() {
+    public void testInvalidAccountNumber() {
         final String accountNumber = "664371495";
         boolean actualResult = BankORC.validateAccountNumber(accountNumber);
         assertFalse(actualResult);
     }
 
+    /**
+     * Method to validate illegible account number.
+     */
     @Test
-    public void test_illegibleAccountNumber() {
+    public void testIllegibleAccountNumber() {
         final StringBuilder number = new StringBuilder();
         number.append("    _  _  _  _  _  _     _ ");
         number.append("|_||_|| || ||_   |  |  | _ ");
@@ -54,29 +70,41 @@ public class BankORCTest {
         assertEquals("49006771? ILL", result);
     }
 
+    /**
+     * Method to validate the value given a key.
+     */
     @Test
-    public void test_getTheKeyForANumber() {
+    public void testGetTheKeyForANumber() {
         final String numberToValidate = " _  _||_ ";
         final int result = BankORC.getKeysForNumber(numberToValidate);
         assertEquals(2, result);
     }
 
+    /**
+     * Method to validate the generated format number for valid account number.
+     */
     @Test
-    public void test_generateFormatNumberForValidAccountNumber() {
+    public void testGenerateFormatNumberForValidAccountNumber() {
         final StringBuilder accountNumber = new StringBuilder("457508000");
         final String result = BankORC.generateFormatNumber(accountNumber);
         assertEquals("457508000", result);
     }
 
+    /**
+     * Method to validate the generated format number for invalid account number.
+     */
     @Test
-    public void test_generateFormatNumberForInvalidAccountNumber() {
+    public void testGenerateFormatNumberForInvalidAccountNumber() {
         final StringBuilder accountNumber = new StringBuilder("664371495");
         final String result = BankORC.generateFormatNumber(accountNumber);
         assertEquals("664371495 ERR", result);
     }
 
+    /**
+     * Method to validate the generated format number for account number with illegible characters.
+     */
     @Test
-    public void test_generateFormatNumberForAccountNumberWithIllegibleCharacters() {
+    public void testGenerateFormatNumberForAccountNumberWithIllegibleCharacters() {
         final StringBuilder accountNumber = new StringBuilder("86110??36");
         final String result = BankORC.generateFormatNumber(accountNumber);
         assertEquals("86110??36 ILL", result);
